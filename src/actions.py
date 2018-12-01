18cfc757949caba164bddb0e9c5c97933e24fb31
xxcandiez <josephdong1998@gmail.com>
2018-12-01 10:57:59 -0500

    added docs to actions.py

---
+++ src/actions.py
@@ -0,0 +1,32 @@
+def click(game_state, loc):
+    """
+    list<list<dict>>, list<int> -> list<list<dict>>
+    modifies game_state and returns modified game_state after the cell is
+    clicked
+    :param game_state: game_state as defined in generator.py
+    :param loc: the (x, y) location of the cell you want to click
+    :returns: gamestate after the cell at provided loc is clicked
+    """
+    return None
+
+def flag(game_state, loc):
+    """
+    list<list<dict>>, list<int> -> list<list<dict>>
+    modifies game_state and returns modified game_state after the cell is
+    flagged
+    :param game_state: game_state as defined in generator.py
+    :param loc: the (x, y) location of the cell you want to click
+    :returns: game_state after the cell at loc is flagged
+    """
+    return None
+
+def auto(game_state, loc):
+    """
+    list<list<dict>>, list<int> -> list<list<dict>>
+    modifies game_state and returns modified game_state after auto is invoked
+    on the cell
+    :param game_state: game state as defined in generator.py
+    :param loc: the (x, y) location of the cell you want to invoke auto on
+    :returns: game_state after auto is invoked on the cell
+    """
+    return None
