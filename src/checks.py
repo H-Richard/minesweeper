8e6c577147f14e78f44d9f4726a6c91e21a3e0db
H-Richard <r.hong@protonmail.com>
2018-12-01 15:03:35 -0500

    checker

--- src/checks.py
+++ src/checks.py
@@ -22,15 +22,7 @@ def won(gamestate: [[{}]]) -> bool:
         for block in row:
             if block['state'] == 'clicked' and block['mine'] == 1:
                 return False
-            if block['state'] == 'clicked' and block['mine'] != 1:
-                pass
+            if block['state'] == '' and block['mine'] != 0:
+                return False
     return True
-
-
-
-gamestate = [[{'mine': 0, 'state': 'clicked', 'neigh': 1}, {'mine': 1, 'state': '', 'neigh': 0}], [{'mine': 0, 'state': 'clicked', 'neigh': 1}, {'mine': 0, 'state': 'clicked', 'neigh': 1}]]
-
-if __name__ == "__main__":
-    print(lost(gamestate))
-    print(won(gamestate))
 
